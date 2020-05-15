6eb6a5334fd17e390b4fa8b924514386a8d09957
Will Chatterson <wjchat@umich.edu>
2020-05-15 19:13:37 -0400

    clear cache

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -87,9 +87,14 @@ const Layout = ({ children }) => {
     }
   return (
     <>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap');
-</style>
+      <head>
+       <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap');
+        </style>
+      </head>
       <div ref = {div=>front=div}>
       {largeNav ? <LargeNav transitionOut = {() => transitionOut()}   toggleNav = {() => toggleLarge(false)} /> :""}
       <Header
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,10 +4,12 @@ import Insta from "../components/Insta.jsx"
 import Values from "../components/values.jsx"
 import "../styles/about.scss"
 import {graphql, useStaticQuery} from "gatsby"
+import SEO from '../components/seo.js'
 
 
 const AboutPage = props =>{
     return(<Layout>
+       <SEO title = "about" />
         <div className = "aboutContainer">
             <h1>About Us</h1>
             <Values />
