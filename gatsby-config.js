b0a7534358ec9a224b913f530c971eb01c33cc23
Will Chatterson <wjchat@umich.edu>
2020-05-25 16:59:29 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,30 @@ module.exports = {
     author: `Will`,
   },
   plugins: [
+{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-116081759-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "https://master.d3amzip69qnu33.amplifyapp.com/",
+      },
+    },
+
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-sass",
       `gatsby-plugin-transition-link`,
